71d3d4341fd9fa33db07b2f34b3df22f4f85424e
Bartek <bartlomiej.stapor@gmail.com>
2018-01-20 00:25:37 +0100

    Script 7 done

--- Python/Skrypt7OK/skrypt7.py
+++ Python/Skrypt7OK/skrypt7.py
@@ -1,81 +1,28 @@
 #!/usr/bin/python
 # Bartlomiej Stapor gr 1
+import subprocess, sys
 
-import argparse
-import glob, os
-import subprocess
-
-parser = argparse.ArgumentParser(description='Arg parser')
-
-parser.add_argument('-d', action="store_true", dest='d', default=False)
-parser.add_argument("file", nargs='+')
-dir_tab = []
-args = parser.parse_args()
+dir_list = []
 
 def find():
-    if (not file.startswith("/")):
-        # print("inside")
-        # print("adding")
-        dir_tab.append(file)
-        # print(dir_tab)
-        # print(dir_tab)
-    searched_phrase = file
-    # print(searched_phrase)
-    # print(dir_tab)
-    for i in dir_tab:
-        bashCommand = "grep -rn " + str(i)
-        # print(bashCommand)
-        process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-        output, error = process.communicate()
-        print(output)
-
-if args.d:
-    for arg in args.file:
-
-        # print(arg)
-        file = str(arg)
-        # print(file.startswith("/"))
-        find()
-
-
-
-
-
-
-#
-# if args.d:
-#     for arg in args.file:
-#
-#         # print(arg)
-#         file = str(arg)
-#         # print(file.startswith("/"))
-#         if (file.startswith("/")):
-#             # print("inside")
-#
-#             dir_tab.append(file)
-#             # print(dir_tab)
-#         else:
-#             searched_phrase = file
-#             # print(searched_phrase)
-#             # print(dir_tab)
-#             for i in dir_tab:
-#                 os.chdir(i)
-#                 # print("chdir set to: " + i)
-#                 for j in glob.glob(searched_phrase):
-#                     print("In folder " + i + ":")
-#                     bashCommand = "grep"
-#                     print(j)
-#             dir_tab = []
-
-
-
-# file = str(args.file)
-# fileLen = len(file)
-# print(file[2:fileLen-2])
-# os.chdir(file[2:fileLen-2])
-# for file in glob.glob("*.txt"):
-#     print(file)
-
-# for file in os.listdir("/asfsafsa"):
-#     if file.endswith(".txt"):
-#         print(os.path.join("/asfsafsa", file))
+    for arg in sys.argv:
+        if arg != "-d":
+            temp = str(arg)
+            if temp.startswith("/"):
+                # print("Appending: " + arg)
+                dir_list.append(arg)
+            if not temp.startswith("/"):
+                if dir_list:
+                    temp = str(arg)
+                    if not temp.startswith("-"):
+                        for i in dir_list:
+                            # print("Procesuje: " + i + "z dir_list")
+                            bashCommand = "grep -rn " + arg + " " + i
+                            # print("Starting bashcommand: " + bashCommand)
+                            process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+                            output, error = process.communicate()
+                            print("Fraza " + arg + " znaleziona w :")
+                            print(output)
+                        dir_list [:]= []
+    print(dir_list)
+find()
